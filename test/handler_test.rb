0c86b5f4eab1e705b24aeb8bdca8661527b673ef
Jessica Goulding <jgoulding2@gmail.com>
2014-06-20 16:01:07 -0600

    adds tests, RAKE file, CSV, handler

---
+++ test/handler_test.rb
@@ -0,0 +1,16 @@
+require_relative './test_helper'
+require_relative '../lib/queue'
+
+class HandlerTest < Minitest::Test
+
+  def test_can_search_case_insensitive_data
+    skip
+  end
+
+  def test_can_differentiate_between_first_and_last_name
+    skip
+  end
+
+  def 
+
+end
---
+++ test/queue_test.rb
@@ -0,0 +1,33 @@
+require_relative './test_helper'
+require_relative '../lib/queue'
+
+class QueueTest < Minitest::Test
+
+  def test_can_load_file?
+  end
+
+  def test_can_load_general_help_instructions
+  end
+
+  def test_can_load_help_command_instructions
+  end
+
+  def test_can_count_record_output_in_current_queue
+  end
+
+  def test_can_clear_the_queue?
+  end
+
+  def test_can_print_out_the_queue_with_header_data
+  end
+
+  def test_can_print_by_attribute
+  end
+
+  def test_can_save_queue_to_csv
+  end
+
+  def test_can_search_csv_by_attribute
+  end
+
+end
