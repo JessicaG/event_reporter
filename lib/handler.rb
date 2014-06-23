3337f8ddeb4a0b7953735a99a296b964684cd49a
Eric Fransen <ericfransen@Erics-MacBook-Pro.local>
2014-06-23 10:41:50 -0600

    adds methods to handler: state, zip, phone_number, email_addres WIP

--- lib/handler.rb
+++ lib/handler.rb
@@ -13,19 +13,6 @@ class Handler
     name = row[:first_name]
     end
   end
-end
-
-  def clean_zipcode(zipcode)
-    if zipcode.nil?
-      "00000"
-    elsif zipcode.length < 5
-      zipcode.rjust(5,"0")
-    elsif zipcode.length > 5
-      zipcode[0..4]
-    else
-      zipcode
-    end
-  end
 
   def clean_first_name(first_Name)
     first_Name.downcase.capitalize
@@ -35,38 +22,29 @@ end
     last_Name.downcase.capitalize
   end
 
-  # def address_builder(street, city, state)
-  #   puts "#{street}, #{city}, #{state}"
-  # end
-
-
-  ## need to clean up more##
+  #refactor?#
   def clean_home_phone(homephone)
+    homephone.to_s.gsub(".()-,+_ ")
     if homephone.length < 10
       "0000000000"
-    elsif homephone.length < 11
-      homephone.ljust(11,"1")
-    elsif homephone.length
-    elsif homephone.length > 10
-      homephone[0..4]
+    elsif homephone.length >= 11
+      homephone[-9..-1]
     else
       homephone
     end
-
-  def clean_email_address
-    ##gem?
   end
-end
-#
-# puts "EventReporter initialized."
-#
-# contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-#
-# contents.each do |row|
-#   first_name = clean_first_name(row[:first_name])
-#   last_name = clean_last_name(row[:last_name])
-#   address = address_builder(row[:address])
-#   zipcode = clean_zipcode(row[:zipcode])
-#
-#   puts "#{last_name}, #{first_name}, #{address}, #{zipcode}"
+
+  #is there a gem for this?#
+  def clean_email_address(Email_Address)
+    email_address.include!(@ .)
+  end
+
+  def clean_state(State)
+    state.upcase
+  end
+
+  def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5,"0")[0..4]
+  end
+
 end
