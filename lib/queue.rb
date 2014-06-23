417c148cc81115c45110f362a8f4eef2389c30de
Eric Fransen <ericfransen@Erics-MacBook-Pro.local>
2014-06-23 10:02:55 -0600

    moves command methods

--- lib/queue.rb
+++ lib/queue.rb
@@ -29,3 +29,37 @@ end
 #  end
 # end
 # Files
+
+
+
+def help
+  #output listing of available commands
+end
+
+def help_command
+  #output description of how to use the specific command
+end
+
+def queue_count
+  #output how many records are in the current queue
+end
+
+def queue_clear
+  #clear queue
+end
+
+def queue_print
+  #print out tab-delimited data table with header row (lastname, firstname, etc)
+end
+
+def queue_print_by_attribute
+  #print data table sorted by specified attribute
+end
+
+def queue_save_to_filename
+  #export current queue to specified filename as a CSV
+end
+
+def attribute_criteria
+  #load queue with all records matching criteria for the given attribute (ex. find zipcode 11111)
+end
